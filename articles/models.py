662521100b3e723cd1fb548d77a28c9ee8c6c35f
Sanjay <sanjaysj6282@gmail.com>
2022-06-26 22:10:11 +0530

    migrations

--- articles/models.py
+++ articles/models.py
@@ -7,3 +7,7 @@ class Articles(models.Model):
     date=models.DateTimeField(auto_now=True)
     # Add in thumbnail later
     # Add in author later
+    
+# To make migration file to track the change in models
+#  python manage.py makemigrations
+#  python manage.py migrate
