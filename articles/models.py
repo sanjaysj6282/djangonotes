ccf27696452d1ce6d02fda40d55ac713535aec94
Sanjay <sanjaysj6282@gmail.com>
2022-06-26 22:37:58 +0530

    admin access

--- articles/models.py
+++ articles/models.py
@@ -7,7 +7,10 @@ class Articles(models.Model):
     date=models.DateTimeField(auto_now=True)
     # Add in thumbnail later
     # Add in author later
-    
+
+def __str__(self):
+    return self.title
+
 # To make migration file to track the change in models
 #  python manage.py makemigrations
 #  python manage.py migrate
