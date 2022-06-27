3c052e1fc8f351bf0f012348903c29915eb2d1fa
Sanjay <sanjaysj6282@gmail.com>
2022-06-28 00:02:20 +0530

    named urls

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name='articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    path('article_list/', views.article_list),
-    path('article_details/<slug>', views.article_details)
+    path('', views.article_list, name="list"),
+    # path('article_list/', views.article_list),
+    path('article_details/<slug>', views.article_details, name="details")
 ]
