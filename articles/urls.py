a566bd2f508fe8a22a5f676fa3ffcaacaebeeee6
Sanjay <sanjaysj6282@gmail.com>
2022-06-27 23:00:35 +0530

    slugging

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list),
-    path('article_detials/', views.article_details)
+    path('article_list/', views.article_list),
+    path('article_details/<slug>', views.article_details)
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Articles
 
-def article_list(request):
-    return render(request, "articles/articles_list.html")
 
-def article_details(request):
+def article_list(request):
     articles=Articles.objects.all().order_by('date')
-    return render(request, "articles/article_details.html", {'articles': articles})
+    return render(request, "articles/articles_list.html", {'articles': articles})
+
+def article_details(request, slug):
+    return HttpResponse(slug)
+    # return render(request, "articles/article_details.html")
