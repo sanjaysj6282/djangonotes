b047dabcfb4b4dad96102c6f1b8e824bf7ab3d19
Sanjay <sanjaysj6282@gmail.com>
2022-06-28 20:58:08 +0530

    to add login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
 def signup_view(request):
@@ -14,3 +14,16 @@ def signup_view(request):
         form=UserCreationForm()
     
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method=='POST':
+        # data is not the 1st parameter of fn by default
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log th user
+            return redirect('articles:list')
+    else:
+        form=AuthenticationForm()
+    
+    return render(request, 'accounts/login.html', {'form': form})
+            
