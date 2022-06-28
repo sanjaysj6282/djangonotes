2955d205f1983140ed5f3378d84f4b55dc136de0
Sanjay <sanjaysj6282@gmail.com>
2022-06-28 23:46:01 +0530

    redirecting functionality

--- djangonotes/urls.py
+++ djangonotes/urls.py
@@ -5,13 +5,15 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
+    path('', article_views.article_list, name="home"),
+    path('about/', views.about),
+    
     path('articles/', include('articles.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('about/', views.about)
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
