da0e81db94d92d57fc08668768d8a9ec16e81613
Sanjay <sanjaysj6282@gmail.com>
2022-06-28 19:50:20 +0530

    added user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    if request.method=='POST':
+        # req.POST is to acccess post data
+        form=UserCreationForm(request.POST)
+        # it validates the form --> is the username ok etc
+        if form.is_valid():
+            form.save()
+            # log the user
+            return redirect('articles:list')
+    else:
+        form=UserCreationForm()
+    
+    return render(request, 'accounts/signup.html', {'form': form})
